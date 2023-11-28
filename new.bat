b9ae1035a7670a0018bc9f4b5bec7478b708ba1c
1666475026 <1666475026@qq.com>
2023-11-28 17:25:30 +0800

    后台项目初始化

---
+++ new.bat
@@ -0,0 +1,7 @@
+echo "# spring-Boot" >> README.md
+  git init
+  git add README.md
+  git commit -m "first commit"
+  git branch -M main
+  git remote add origin https://github.com/1666475026/spring-Boot.git
+  git push -u origin main
